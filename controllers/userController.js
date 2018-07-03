e87736b6eda12f48e9de968ae550894ede661736
Victor Faucon <vicfaucon@gmail.com>
2018-07-02 17:17:30 -0700

    Database wip

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+const pg = require('pg');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+const connectionString = 'postgres://grgrkypm:Wj-hDJsZaHn-pUoCSW_ON_z3JED4ZnPB@baasu.db.elephantsql.com:5432/grgrkypm'
+
+const client = new pg.Client({ connectionString })
+
+client.connect(function (err) {
+    if (err) {
+      console.log("client connect: ", err);
+    } else {
+      console.log('hi');
+    }
+  })
+
+
+  const userController = {
+
+    registerUser: (req, res, next) => {
+        let { username, date, bio, picture } = req.body;
+        let q = `INSERT INTO users VALUES (${username}, ${date}, ${bio}, ${picture});`
+        client.query(q, (req, res, next) => {
+            if (err) {
+                console.log('error:', err);
+                res.end();
+              } else {
+                res.send(results);
+              }
+        });
+
+
+    }
+
+
+
+
+  }
