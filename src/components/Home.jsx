28929d4dd0e81f0ec048a261f70b0aad0489a70e
Jessica Skok <jessicaskok@gmail.com>
2018-07-03 21:54:58 -0700

    still working on home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,28 @@ import { connect } from 'react-redux';
 const Home = () => {
 
   return (
-    <div id="login-container">
+    <div id="container">
+    <div id="homePic">
       <button type="button"><a href="https://github.com/login/oauth/authorize?client_id=d337730ee82c0f67d053&scope=user">Log in with Github</a></button>
+    </div>
     </div>
   );
 };
 
 
 
+function mapStateToProps(state, props) {
+  return {
+    login: state.reducers.sampleusers.userName,
+    avatar_url: state.reducers.sampleusers.userPhoto,
+    followers: state.reducers.sampleusers.userBio
+  }
+}
 
-export default Home;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -19,6 +19,8 @@ const homeInitialState = {
       userBio: 'PERCH 4 LYFE'
     }
   ]
+
+  
 }
 
 export default reducers = (state = homeInitialState, action) => {
