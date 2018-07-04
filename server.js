f27df0ba807a7ebc3cfc8d4dd5d9e34b63c828b7
William Adamowicz <william.adamowicz@gmail.com>
2018-07-04 16:08:31 -0700

    add cookie logic, conditional render

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const userController = require('./controllers/userController');
 const cookieController = require('./controllers/cookieController');
-
+const cookieParser = require('cookie-parser')
 // Postgresql stuff
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://grgrkypm:Wj-hDJsZaHn-pUoCSW_ON_z3JED4ZnPB@baasu.db.elephantsql.com:5432/grgrkypm';
@@ -19,17 +19,31 @@ app.use((req, res, next) => {
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+  console.log('cookies yo', req.cookies)
+  if (req.cookies.token) {
+    console.log('doing stuff')
+  }
   res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
 
+app.get('/main.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/main.js'));
+});
+
 // OAuth Login
-app.get('/login/oauth', userController.getCodeAndPost, userController.registerUser, cookieController.setTokenCookie, (req, res) => {
-  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+app.get('/login/oauth', 
+  userController.getCodeAndPost, 
+  userController.registerUser, 
+  cookieController.setTokenCookie, 
+  (req, res) => {
+    console.log('rerouting home')
+    res.redirect('/')
 });
 
 // Display profile
