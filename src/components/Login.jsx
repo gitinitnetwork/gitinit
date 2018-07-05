b7c4c4bcce6bb3bd09ad66c3309ad83b7cdb2a61
Jessica Skok <jessicaskok@gmail.com>
2018-07-05 15:27:39 -0700

    some pic stuff

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import logo from '../../assets/git-init-logo.png'
 const Login = () => {
   return (
     <div id="login-container">
+      <img src={logo} width="200" />
       <button type="button"><a href="https://github.com/login/oauth/authorize?client_id=d337730ee82c0f67d053&scope=user">Log in with Github</a></button>
     </div>
   );
--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import logo from '../../assets/git-init-logo.png'
 
 const mapStateToProps = store => ({
   pendingUsers: store.matches.pendingUsers,
@@ -11,7 +12,7 @@ const Voting = (props) => {
     <div id="voting-container">
       <div id="route-buttons">
         <Link to="/settings"><div className="settings-link"><button><i className="fas fa-cog fa-2x" /></button></div></Link>
-        <Link to="/matches"><div className="matches-link"><button><i className="far fa-heart fa-2x" /></button></div></Link>
+        <Link to="/matches"><div className="matches-link"><button><img src={logo} width="80" /></button></div></Link>
       </div>
       <img id="homePic" alt="Vote On Me" src={props.pendingUsers[0].avatar_url} />
       <div id="voting-buttons">
